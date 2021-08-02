a7fc1fba7128a30ead458ea3a8dfb8d3c0fe5407
Zhenya Zhdanov <57041103+acid26sk@users.noreply.github.com>
2021-08-02 22:08:40 +0300

    Выполнил ДЗ_11

---
+++ src/HW_11/Orange.java
@@ -0,0 +1,14 @@
+package HW_11;
+
+public class Orange extends Fruit {
+
+    Orange() {
+        super(1.5f);
+    }
+
+    @Override
+    public String toString(){
+        return "апельсины";
+    }
+
+}
